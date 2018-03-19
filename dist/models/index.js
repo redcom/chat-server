13b8c3b09ce469f066592c512c6ccb8b77a7a213
Moraru Razvan <redcom@gmail.com>
2018-03-19 06:51:53 +0100

    init server

---
+++ dist/models/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _sequelize = require('sequelize');
+
+var _sequelize2 = _interopRequireDefault(_sequelize);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const sequelize = new _sequelize2.default(process.env.TEST_DB || 'slack', 'postgres', 'postgres', {
+  dialect: 'postgres',
+  operatorsAliases: _sequelize2.default.Op,
+  define: {
+    underscored: true
+  }
+});
+
+const models = {
+  User: sequelize.import('./user'),
+  Channel: sequelize.import('./channel'),
+  Message: sequelize.import('./message'),
+  Team: sequelize.import('./team'),
+  Member: sequelize.import('./member'),
+  DirectMessage: sequelize.import('./directMessage'),
+  PCMember: sequelize.import('./pcmember')
+};
+
+Object.keys(models).forEach(modelName => {
+  if ('associate' in models[modelName]) {
+    models[modelName].associate(models);
+  }
+});
+
+models.sequelize = sequelize;
+models.Sequelize = _sequelize2.default;
+models.op = _sequelize2.default.Op;
+
+exports.default = models;
